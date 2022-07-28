a2398ac437ae7eede1f71f9360e5147a102c1397
Gulsher <apunichgulsher@gmail.com>
2022-07-28 12:28:59 +0530

    First Commit

--- app/src/main/java/com/blogspot/bunnylists/chitchat/ProfileMenu/ProfileActivity.kt
+++ app/src/main/java/com/blogspot/bunnylists/chitchat/ProfileMenu/ProfileActivity.kt
@@ -2,11 +2,13 @@ package com.blogspot.bunnylists.chitchat.ProfileMenu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.blogspot.bunnylists.chitchat.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        Toast.makeText(this, "I am joking!!", Toast.LENGTH_SHORT).show()
     }
 }
