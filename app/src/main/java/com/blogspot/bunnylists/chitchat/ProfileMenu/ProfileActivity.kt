7a4b80197de6881b2df5abb38c9d5bac672936d8
Gulsher <apunichgulsher@gmail.com>
2022-07-28 16:57:23 +0530

    ProfileActivity updated

--- app/src/main/java/com/blogspot/bunnylists/chitchat/ProfileMenu/ProfileActivity.kt
+++ app/src/main/java/com/blogspot/bunnylists/chitchat/ProfileMenu/ProfileActivity.kt
@@ -9,6 +9,5 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
-
     }
 }
